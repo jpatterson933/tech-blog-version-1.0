10938509a5cb577d63615b18e8f2f68585067f23
Jeffery Patterson <jpatterson933@ucla.edu>
2021-04-08 15:59:06 -0700

    I can now filter through posts and users and grab their ids
    however I have no clue how to use the currently logged in user, grab their id, and then filter through the posts where the user_id matches the logged in user id

--- controllers/main.js
+++ controllers/main.js
@@ -35,10 +35,50 @@ router.get('/', async (req, res) => {
 router.get('/dash', async (req, res) => {
     try {
         const postData = await Post.findAll();
-        console.log(postData)
+        const userData = await User.findAll();
+        
         const myPost = postData.map(posts => posts.get({ plain: true }));
-        console.log(myPost, "this is mapped")
-        res.render('dash', { myPost, loggedIn: req.session.loggedIn })
+
+        const post = postData.filter( posts => {
+            posts.get({ plain: true });
+
+            const postId = posts.dataValues.user_id;
+
+            console.log(posts.dataValues.user_id, "user id")
+            return postId;
+        })
+        
+        const user = userData.filter( users => {
+            users.get({ plain: true })
+            const userId = users.dataValues.id;
+            console.log(users.dataValues.id, "id")
+            return userId;
+        });
+        console.log(user, "another test")
+
+        // const user = userData.map(users => users.get({ plain: true }));
+
+
+        // for (let i = 0; i < user.length; i++) {
+        //     console.log(user[i].id, "this works")
+        // }
+        // console.log(user[1].id, "testing 123----------------------------------------")
+        // for (let i = 0; i < myPost.length; i++) {
+        //     console.log(myPost[i].user_id, "post id here")
+        // }
+        // console.log(myPost[3].user_id, "--------------------------1234-----------------------")
+
+        
+        // console.log(myPost[1], "post data array")
+        // console.log(user, "user data array")
+
+        
+            // console.log(postData)
+            // console.log(myPost, "this is mapped")
+            res.render('dash', { myPost, loggedIn: req.session.loggedIn })
+        
+
+        
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
