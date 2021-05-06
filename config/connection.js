8c18359be9609c75cb3a6e224d9562735d79966d
Jeffery Patterson <jpatterson933@ucla.edu>
2021-05-06 12:46:23 -0700

    added Jaws DB to connection.js in config folder

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,25 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    //host for mysql
-    host: 'localhost',
-    //the dialect configuration lets us know which database we are using
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(
+      process.env.JAWSDB_URL
+    )
+} else {
+  
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      //host for mysql
+      host: 'localhost',
+      //the dialect configuration lets us know which database we are using
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+
+}
 
 module.exports = sequelize;
