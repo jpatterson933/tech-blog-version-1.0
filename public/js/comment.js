4acf844d5143f3bfc2bce538a812fd456133b7d0
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-07-15 10:47:24 -0700

    extracted fetchnew comment request into its own async function

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,3 +1,16 @@
+async function fetchNewComment(comment, username, post_id) {
+    try {
+        const response = fetch('/api/comment', {
+            method: 'POST',
+            body: JSON.stringify({ comment, username, post_id }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        return response;
+    } catch (err) {
+        console.error(err);
+    };
+};
+
 //this function is reponsible for creating our comments and storing them in our database
 const createComment = async (event) => {
     event.preventDefault();
@@ -10,26 +23,16 @@ const createComment = async (event) => {
     const post_id = element.dataset.postid;
 
     if (comment) {
-        fetch('/api/comment', {
-            method: 'POST',
-            body: JSON.stringify({ comment, username, post_id }),
-            headers: { 'Content-Type': 'application/json' },
-        })
-            .then(response => {
-                if (response.ok) {
-                    alert("Your comment has been posted!");
-                    document.location.reload();
-                    return response.json();
-                } else {
-                    alert("Comment failed!");
-                }
-            })
-            .catch(err => {
-                
-                console.log(err);
-            })
-    }
-}
+        let response = await fetchNewComment(comment, username, post_id);
+        if (response.ok) {
+            alert("Your comment has been posted!");
+            document.location.reload();
+            return response.json();
+        } else {
+            alert("Comment failed!");
+        };
+    };
+};
 
 document
     .querySelector(".comment")
